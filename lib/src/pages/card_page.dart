694c5470cf567a4ef87e0605db816d169fa100eb
Barbara Sanser <bsanser@gmail.com>
2020-03-17 17:40:54 +0000

    feat: created card component

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardType1(),
+          SizedBox(height: 30.0),
+          _cardType2(),
+          SizedBox(height: 30.0),
+          _cardType1(),
+          SizedBox(height: 30.0),
+          _cardType2(),
+          SizedBox(height: 30.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType1() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('The Title'),
+            subtitle: Text('Beatiful places, delightful experiences'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+            placeholder: AssetImage('assets/original.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+
+          // Image(
+          //   image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+          // ),
+          Container(padding: EdgeInsets.all(10.0), child: Text('Hi, Sunset'))
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
+      ),
+    );
+  }
+}
